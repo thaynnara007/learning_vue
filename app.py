3a2050ed90c41a96e02786c2c6db25c891ed28fb
thaynnara007 <thaynnara.goncalves@ccc.ufcg.edu.br>
2019-07-21 22:30:55 -0300

    init vue application

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, jsonify
+from flask_cors import CORS
+
+DEBUG = True
+
+app = Flask(__name__)
+app.config.from_object(__name__)
+
+CORS(app, resources={r'/*': {'origins': '*'}})
+
+@app.route('/ping', methods=['GET'])
+def ping():
+  return jsonify('pong')
+
+if __name__ == '__main__':
+  app.run()
